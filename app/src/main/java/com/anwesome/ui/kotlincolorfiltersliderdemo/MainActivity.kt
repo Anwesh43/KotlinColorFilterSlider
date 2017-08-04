8e2ca3f1a6bee011e774e6e6bdb634cb497bcec2
Anwesh43 <anweshthecool0@gmail.com>
2017-08-04 19:25:12 +0530

    using ColorFilterSliderKotlin in Activity

--- app/src/main/java/com/anwesome/ui/kotlincolorfiltersliderdemo/MainActivity.kt
+++ app/src/main/java/com/anwesome/ui/kotlincolorfiltersliderdemo/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.anwesome.ui.kotlincolorfiltersliderdemo
 
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesome.ui.colorfiltersliderkotlin.ColorFilterSlider
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var bitmap = BitmapFactory.decodeResource(resources,R.drawable.ozil)
+        ColorFilterSlider.create(this,bitmap, arrayOf(Color.GREEN,Color.CYAN,Color.BLACK,Color.BLUE,Color.RED,Color.MAGENTA))
     }
 }
